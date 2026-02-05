2ac81915e5f0906f0d13cf76008b44ad48d53be6
Manon <manon.sella@qonto.com>
2026-02-05 22:11:26 +0100

    adds helpers in movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,18 @@ class Movie < ApplicationRecord
     validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
     validates :description, length: { minimum: 20 }
     validates :theme, presence: true
+
+    def has_enough_reviews?
+        # return self.reviews.size >= 3
+        reviews.size >= 3
+    end
+    # def average_score
+    #     total = 0
+    #     reviews.each do |review|
+    #         total = total + review.rating
+    #     end
+
+    #     score = total / reviews.size
+    #     score
+    # end 
 end
