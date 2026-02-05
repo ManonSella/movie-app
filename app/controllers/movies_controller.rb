13887291f6cfd62cdf26c7f12c154b12f2eb866c
Manon <manon.sella@qonto.com>
2026-02-05 22:06:45 +0100

    adds flash messages for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,8 @@ class MoviesController < ApplicationController
     def update 
         @movie = Movie.find(params[:id])
         if @movie.update(movie_params)
-        redirect_to @movie
+            #flash[:notice] = "Event successfully updated!""
+            redirect_to @movie, notice: "Event successfully updated!"
         else
             render :edit, status: :unprocessable_entity
         end
@@ -29,7 +30,7 @@ class MoviesController < ApplicationController
         # fail params[:movie]
         @movie = Movie.new(movie_params)
         if @movie.save
-           redirect_to @movie
+            redirect_to @movie, notice: "Event successfully created!"
         else 
             render :new, status: :unprocessable_entity
         end
